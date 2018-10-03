7fe342099c81a7479aff104a3221778294169503
luthesebas <luthesebas@gmail.com>
2018-10-03 13:34:59 +0200

    Reorganized project structure

--- src/main/java/de/ka/sl/sight/SightApplication.java
+++ src/main/java/de/ka/sl/sight/SightApplication.java
@@ -1,4 +1,4 @@
-package de.ka.sl.sight.sight;
+package de.ka.sl.sight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/de/ka/sl/sight/SightApplicationTests.java
+++ src/test/java/de/ka/sl/sight/SightApplicationTests.java
@@ -1,4 +1,4 @@
-package de.ka.sl.sight.sight;
+package de.ka.sl.sight;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
