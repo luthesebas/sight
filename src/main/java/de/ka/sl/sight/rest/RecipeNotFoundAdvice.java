dd382500ded099d3e5f25f99e85347ebd4f087cf
luthesebas <luthesebas@gmail.com>
2018-10-03 15:50:08 +0200

    implemented controller advice

---
+++ src/main/java/de/ka/sl/sight/rest/RecipeNotFoundAdvice.java
@@ -0,0 +1,22 @@
+package de.ka.sl.sight.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ */
+@ControllerAdvice
+public class RecipeNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(RecipeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String handel(RecipeNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+}
