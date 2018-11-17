927a3238640bf6e57af42312da20770b77ed6c8c
luthesebas <luthesebas@gmail.com>
2018-11-17 15:00:43 +0100

    Fix

--- src/main/java/de/ka/sl/sight/rest/RecipeController.java
+++ src/main/java/de/ka/sl/sight/rest/RecipeController.java
@@ -3,7 +3,7 @@ package de.ka.sl.sight.rest;
 import de.ka.sl.sight.persistence.Recipe;
 import de.ka.sl.sight.persistence.RecipeDAO;
 import de.ka.sl.sight.rest.mapper.RecipeMapper;
-import de.ka.sl.sight.rest.misc.RecipeNotFoundException;
+import de.ka.sl.sight.rest.misc.recipe.RecipeNotFoundException;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
