4f7f9ee569eed1b7371fcd8c20d3021797bccf83
luthesebas <luthesebas@gmail.com>
2018-11-17 13:49:11 +0100

    Added intruction reference

--- src/main/java/de/ka/sl/sight/persistence/Recipe.java
+++ src/main/java/de/ka/sl/sight/persistence/Recipe.java
@@ -1,9 +1,14 @@
 package de.ka.sl.sight.persistence;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
@@ -12,12 +17,18 @@ import javax.persistence.*;
 @Entity
 @EqualsAndHashCode(of = "id")
 @Table(name = "recipe")
-public class Recipe {
+public final class Recipe {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
+
+    @JsonIgnore
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<Instruction> instructions;
 
     //--------------------------------------
     // Constructors
@@ -28,6 +39,7 @@ public class Recipe {
     public Recipe(String title, String description) {
         this.title = title;
         this.description = description;
+        this.instructions = new LinkedList<>();
     }
 
     //--------------------------------------
@@ -37,6 +49,14 @@ public class Recipe {
     public void updateFrom(Recipe recipe) {
         this.title = recipe.title;
         this.description = recipe.description;
+    }
+
+    public void add(Instruction instruction) {
+        instructions.add(instruction);
+    }
+
+    public void remove(Instruction instruction) {
+        instructions.remove(instruction);
     }
 
     //--------------------------------------
