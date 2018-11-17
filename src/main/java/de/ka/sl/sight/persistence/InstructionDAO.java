72d13ac8e0b28a3568540a21aeb32e0c9b244211
luthesebas <luthesebas@gmail.com>
2018-11-17 16:31:36 +0100

    Find instructions by id and recipe id

--- src/main/java/de/ka/sl/sight/persistence/InstructionDAO.java
+++ src/main/java/de/ka/sl/sight/persistence/InstructionDAO.java
@@ -3,6 +3,7 @@ package de.ka.sl.sight.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  *
@@ -10,5 +11,7 @@ import java.util.List;
 public interface InstructionDAO extends JpaRepository<Instruction, Long> {
 
     List<Instruction> findAllByRecipeId(long recipeId);
+
+    Optional<Instruction> findByIdAndRecipeId(long id, long recipeId);
 
 }
