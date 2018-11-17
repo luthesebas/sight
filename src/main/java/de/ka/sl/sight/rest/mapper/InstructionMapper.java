8810cefd170a2a40ae3825a8e87399eb9bda89e8
luthesebas <luthesebas@gmail.com>
2018-11-17 14:06:32 +0100

    Introduced instruction mapper

---
+++ src/main/java/de/ka/sl/sight/rest/mapper/InstructionMapper.java
@@ -0,0 +1,26 @@
+package de.ka.sl.sight.rest.mapper;
+
+import de.ka.sl.sight.persistence.Instruction;
+import de.ka.sl.sight.rest.InstructionController;
+import de.ka.sl.sight.rest.RecipeController;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+/**
+ *
+ */
+@Component
+public class InstructionMapper implements ResourceAssembler<Instruction, Resource<Instruction>> {
+
+    @Override
+    public Resource<Instruction> toResource(Instruction instruction) {
+        Resource<Instruction> resource = new Resource<>(instruction);
+        resource.add(linkTo(InstructionController.class, instruction.getId()).slash(instruction.getId()).withSelfRel());
+        resource.add(linkTo(InstructionController.class, instruction.getId()).withRel("instructions"));
+        return resource;
+    }
+
+}
