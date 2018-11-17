e371c393686172f51760b588be874369a20d3431
luthesebas <luthesebas@gmail.com>
2018-11-17 14:58:09 +0100

    Renamed SightApplication to App

--- src/main/java/de/ka/sl/sight/App.java
+++ src/main/java/de/ka/sl/sight/App.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SightApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SightApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
+
 }
