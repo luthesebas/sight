0536cce12b19db73b6083ccb4f91c6a4048008a9
luthesebas <luthesebas@gmail.com>
2019-03-23 07:54:46 +0100

    Update AppExceptionAdvice.java

--- src/main/java/de/ka/sl/sight/rest/general/AppExceptionAdvice.java
+++ src/main/java/de/ka/sl/sight/rest/general/AppExceptionAdvice.java
@@ -1,12 +1,14 @@
 package de.ka.sl.sight.rest.general;
 
-import de.ka.sl.sight.rest.resource.instruction.InstructionNotFoundException;
-import de.ka.sl.sight.rest.resource.recipe.RecipeNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
 
 /**
  * @author Sebastian Luther (https://github.com/luthesebas)
@@ -14,18 +16,47 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public final class AppExceptionAdvice {
 
-    @ResponseBody
-    @ExceptionHandler(RecipeNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handel(RecipeNotFoundException ex) {
-        return ex.getMessage();
+    private ExceptionMessage mapToModel(Exception ex) {
+        return new ExceptionMessage(ex.getMessage());
     }
 
+    private ExceptionMessage mapToModel(String message) {
+        return new ExceptionMessage(message);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
     @ResponseBody
-    @ExceptionHandler(InstructionNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handel(InstructionNotFoundException ex) {
-        return ex.getMessage();
+    public ExceptionMessage handel(NotFoundException ex) {
+        return mapToModel(ex);
+    }
+
+    @ExceptionHandler(JsonMappingException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionMessage handel(JsonMappingException ex) {
+        return mapToModel(ex);
+    }
+
+    @ExceptionHandler(JsonProcessingException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionMessage handel(JsonProcessingException ex) {
+        return mapToModel(ex);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionMessage handel(HttpMessageNotReadableException ex) {
+        return mapToModel(ex);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionMessage handel(Exception ex, WebRequest request) {
+        return mapToModel("Oops... something went wrong on our end. Sorry!");
     }
 
 }
