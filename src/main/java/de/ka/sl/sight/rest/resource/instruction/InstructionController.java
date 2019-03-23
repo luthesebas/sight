f539ca615f03a107486424a2ab02bc7ff1413aa4
luthesebas <luthesebas@gmail.com>
2019-03-23 08:07:01 +0100

    Update InstructionController.java

--- src/main/java/de/ka/sl/sight/rest/resource/instruction/InstructionController.java
+++ src/main/java/de/ka/sl/sight/rest/resource/instruction/InstructionController.java
@@ -3,9 +3,14 @@ package de.ka.sl.sight.rest.resource.instruction;
 import de.ka.sl.sight.persistence.instruction.InstructionEntity;
 import de.ka.sl.sight.persistence.instruction.InstructionDAO;
 import de.ka.sl.sight.persistence.recipe.RecipeEntity;
-import de.ka.sl.sight.persistence.recipe.RecipeDAO;
 import de.ka.sl.sight.rest.general.AppException;
+import de.ka.sl.sight.rest.general.NotFoundException;
+import de.ka.sl.sight.rest.general.Path;
 import de.ka.sl.sight.rest.resource.recipe.RecipeNotFoundException;
+import de.ka.sl.sight.rest.resource.recipe.RecipeService;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.apache.bcel.generic.Instruction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
@@ -23,24 +28,14 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
  * @author Sebastian Luther (https://github.com/luthesebas)
  */
 @RestController
-@RequestMapping(value = "/recipes/{recipeId:[0-9]+}/instructions")
+@RequestMapping(value = Path.RECIPES + "/{recipeId:[0-9]+}/instructions")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public final class InstructionController {
 
-    private InstructionDAO instructionDAO;
-    private InstructionMapper instructionMapper;
+    private final InstructionDAO instructionDAO;
+    private final InstructionMapper instructionMapper;
 
-    private RecipeDAO recipeDAO;
-
-    //--------------------------------------
-    // Constructors
-    //--------------------------------------
-
-    public InstructionController(InstructionDAO instructionDAO, InstructionMapper instructionMapper,
-                                 RecipeDAO recipeDAO) {
-        this.instructionDAO = instructionDAO;
-        this.instructionMapper = instructionMapper;
-        this.recipeDAO = recipeDAO;
-    }
+    private final RecipeService recipeService;
 
     //--------------------------------------
     // Methods
@@ -48,23 +43,28 @@ public final class InstructionController {
 
     @GetMapping()
     public Resources<Resource<InstructionEntity>> all(@PathVariable(name = "recipeId") long recipeId) {
-        List<Resource<InstructionEntity>> instructions = instructionDAO.findAllByRecipeId(recipeId).stream()
+        List<Resource<InstructionEntity>> instructions = instructionDAO
+                .findAllByRecipeId(recipeId).stream()
                 .map(instructionMapper::toResource)
                 .collect(Collectors.toList());
         return new Resources<>(instructions, linkTo(InstructionController.class, recipeId).withSelfRel());
     }
 
-    @GetMapping("/{id:[0-9]+}")
+    @GetMapping(Path.ID)
     public Resource<InstructionEntity> read(@PathVariable long id) throws AppException {
-        return instructionMapper.toResource(
-                instructionDAO.findById(id).orElseThrow(() -> new InstructionNotFoundException(id)));
+        Optional<InstructionEntity> optional = instructionDAO.findById(id);
+        if (optional.isPresent()) {
+            return instructionMapper.toResource(optional.get());
+        } else {
+            throw new NotFoundException(Instruction.class, id);
+        }
     }
 
     @PostMapping()
     public ResponseEntity<?> create(@RequestBody InstructionEntity instructionEntity,
                                     @PathVariable(name = "recipeId") long recipeId)
             throws AppException, URISyntaxException {
-        Optional<RecipeEntity> optional = recipeDAO.findById(recipeId);
+        Optional<RecipeEntity> optional = recipeService.get(recipeId);
         if (optional.isPresent()) {
             instructionEntity = instructionDAO.save(instructionEntity);
             instructionEntity.setRecipe(optional.get());
@@ -77,14 +77,15 @@ public final class InstructionController {
         }
     }
 
-    @PutMapping("/{id:[0-9]+}")
+    @PutMapping(Path.ID)
     public ResponseEntity<?> update(@RequestBody InstructionEntity newInstructionEntity,
                                     @PathVariable(name = "recipeId") long recipeId,
                                     @PathVariable(name = "id") long id)
             throws AppException, URISyntaxException {
-        Optional<RecipeEntity> optional = recipeDAO.findById(recipeId);
+        Optional<RecipeEntity> optional = recipeService.get(recipeId);
         if (optional.isPresent()) {
-            InstructionEntity updatedInstructionEntity = instructionDAO.findByIdAndRecipeId(id, recipeId)
+            InstructionEntity updatedInstructionEntity = instructionDAO
+                    .findByIdAndRecipeId(id, recipeId)
                     .map(instruction -> {
                         instruction.updateFrom(newInstructionEntity);
                         return instructionDAO.save(instruction);
@@ -103,7 +104,7 @@ public final class InstructionController {
         }
     }
 
-    @DeleteMapping("/{id:[0-9]+}")
+    @DeleteMapping(Path.ID)
     public ResponseEntity<?> delete(@PathVariable long recipeId, @PathVariable long id) {
         if (instructionDAO.existsById(id)) {
             instructionDAO.deleteById(id);
