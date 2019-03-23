735bcb0cd9d4f4c26803016018a2679a43a255bb
luthesebas <luthesebas@gmail.com>
2019-03-23 11:27:22 +0100

    Update Endpoint.java

--- src/main/java/de/ka/sl/sight/config/Endpoint.java
+++ src/main/java/de/ka/sl/sight/config/Endpoint.java
@@ -5,12 +5,16 @@ package de.ka.sl.sight.config;
  */
 public class Endpoint {
 
+    //TODO Extract values like 'recipes' and 'instructions' from a config file
+
     private Endpoint() {
 
     }
 
-    //TODO Extract values from a config file
     public final static String RECIPES = "/recipes";
+    public final static String RECIPES_ID = RECIPES + "/{recipeId:[0-9]+}";
+
+    public final static String INSTRUCTIONS = "/instructions";
 
     public final static String ID_VAR_NAME = "id";
     public final static String ID = "/{id:[0-9]+}";
