974d14c53c06cbbd9e144752aef7726ee0aec841
luthesebas <luthesebas@gmail.com>
2019-03-23 07:25:05 +0100

    Create NotFoundException.java

---
+++ src/main/java/de/ka/sl/sight/rest/general/NotFoundException.java
@@ -0,0 +1,15 @@
+package de.ka.sl.sight.rest.general;
+
+/**
+ * @author Sebastian Luther (@url(https://github.com/luthesebas))
+ */
+public class NotFoundException extends AppException {
+
+    public NotFoundException(Class clazz, long id) {
+        super(String.format(
+                "Could not find %s with id '%s'.",
+                clazz.getSimpleName().toLowerCase(), id
+        ));
+    }
+
+}
