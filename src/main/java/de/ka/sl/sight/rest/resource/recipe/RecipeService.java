7111112162cce45dbd95ef0106b4298702d76bdc
luthesebas <luthesebas@gmail.com>
2019-03-23 08:42:41 +0100

    Update RecipeService.java

--- src/main/java/de/ka/sl/sight/rest/resource/recipe/RecipeService.java
+++ src/main/java/de/ka/sl/sight/rest/resource/recipe/RecipeService.java
@@ -2,35 +2,64 @@ package de.ka.sl.sight.rest.resource.recipe;
 
 import de.ka.sl.sight.persistence.recipe.RecipeDAO;
 import de.ka.sl.sight.persistence.recipe.RecipeEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 /**
  * @author Sebastian Luther (@url(https://github.com/luthesebas))
  */
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RecipeService {
 
-    private RecipeDAO recipeDAO;
-
-    //--------------------------------------
-    // Constructors
-    //--------------------------------------
-
-    @Autowired
-    public RecipeService(RecipeDAO recipeDAO) {
-        this.recipeDAO = recipeDAO;
-    }
+    private final RecipeDAO recipeDAO;
+    private final RecipeMapper recipeMapper;
 
     //--------------------------------------
     // Methods
     //--------------------------------------
 
+    public Resource<RecipeEntity> asResource(RecipeEntity entity) {
+        return recipeMapper.toResource(entity);
+    }
+
+    public List<Resource<RecipeEntity>> asResource(List<RecipeEntity> entities) {
+        return entities.stream()
+                .map(recipeMapper::toResource)
+                .collect(Collectors.toList());
+    }
+
+    public Resources<Resource<RecipeEntity>> asResource(List<RecipeEntity> entities, Class controller) {
+        List<Resource<RecipeEntity>> resources = entities.stream()
+                .map(recipeMapper::toResource)
+                .collect(Collectors.toList());
+        return new Resources<>(resources, linkTo(controller).withSelfRel());
+    }
+    
+
     public RecipeEntity save(RecipeEntity recipeEntity) {
         return recipeDAO.save(recipeEntity);
+    }
+
+    public RecipeEntity update(long recipeId, RecipeEntity newRecipeEntity) {
+        return get(recipeId)
+                .map(recipe -> {
+                    recipe.updateFrom(newRecipeEntity);
+                    return save(recipe);
+                })
+                .orElseGet(() -> {
+                    //newRecipeEntity.setId(id);
+                    return save(newRecipeEntity);
+                });
     }
 
     public Optional<RecipeEntity> get(long id) {
@@ -46,7 +75,9 @@ public class RecipeService {
     }
 
     public void delete(long id) {
-        recipeDAO.deleteById(id);
+        if (exists(id)) {
+            recipeDAO.deleteById(id);
+        }
     }
 
 }
