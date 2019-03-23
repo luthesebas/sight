f6e6a1211832f74f9526f6609fca722167045ac2
luthesebas <luthesebas@gmail.com>
2019-03-23 07:12:22 +0100

    Create Path.java

---
+++ src/main/java/de/ka/sl/sight/rest/general/Path.java
@@ -0,0 +1,16 @@
+package de.ka.sl.sight.rest.general;
+
+/**
+ * @author Sebastian Luther (@url(https://github.com/luthesebas))
+ */
+public class Path {
+
+    private Path() {
+
+    }
+
+    public final static String RECIPES = "/recipes";
+
+    public final static String ID = "/{id:[0-9]+}";
+
+}
