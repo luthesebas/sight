007721c407f22aeda9455f468f8d2f82dc3de302
luthesebas <luthesebas@gmail.com>
2019-09-02 18:20:08 +0200

    Create CreateRecipe.java

---
+++ src/main/java/de/ka/sl/sight/rest/resource/recipe/model/CreateRecipe.java
@@ -0,0 +1,12 @@
+package de.ka.sl.sight.rest.resource.recipe.model;
+
+import lombok.Data;
+
+/** @author Sebastian Luther (https://github.com/luthesebas) */
+@Data
+public class CreateRecipe {
+
+    private String title;
+    private String description;
+
+}
