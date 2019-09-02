c8712dc0a8ce562efeb364bc061bd999e6064843
luthesebas <luthesebas@gmail.com>
2019-09-02 11:46:00 +0200

    Update InstructionMapper.java

--- src/main/java/de/ka/sl/sight/rest/resource/instruction/InstructionMapper.java
+++ src/main/java/de/ka/sl/sight/rest/resource/instruction/InstructionMapper.java
@@ -1,15 +1,16 @@
 package de.ka.sl.sight.rest.resource.instruction;
 
 import de.ka.sl.sight.persistence.instruction.InstructionEntity;
-import de.ka.sl.sight.rest.resource.IMapper;
+import de.ka.sl.sight.rest.resource.IModelMapper;
 import org.springframework.stereotype.Component;
 
 /** @author Sebastian Luther (https://github.com/luthesebas) */
 @Component
-public final class InstructionMapper implements IMapper<InstructionEntity, InstructionEntity> {
+public final class InstructionMapper implements IModelMapper<InstructionEntity, InstructionEntity> {
 
     @Override
     public InstructionEntity mapToModel (InstructionEntity model) {
         return model;
     }
+
 }
