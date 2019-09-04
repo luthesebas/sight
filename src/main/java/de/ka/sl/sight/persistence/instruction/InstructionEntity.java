a715598ba4caa36717bc506a0a93ff8f6114dce3
luthesebas <luthesebas@gmail.com>
2019-09-04 18:09:05 +0200

    Add Unique Constraint for recipe and step field

--- src/main/java/de/ka/sl/sight/persistence/instruction/InstructionEntity.java
+++ src/main/java/de/ka/sl/sight/persistence/instruction/InstructionEntity.java
@@ -9,17 +9,17 @@ import lombok.ToString;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /** @author Sebastian Luther (https://github.com/luthesebas) */
 @Data
-@ToString(exclude = {"recipe"})
+@ToString(callSuper = true, exclude = {"recipe"})
 @EqualsAndHashCode(callSuper = true, exclude = {"recipe"})
 @Entity
-@Table(name = "instruction")
+@Table(
+    name = "instruction",
+    uniqueConstraints = { @UniqueConstraint(columnNames = {"recipe_id", "step"}) }
+)
 public final class InstructionEntity extends IEntity {
 
     private int step;
@@ -27,8 +27,9 @@ public final class InstructionEntity extends IEntity {
     private int durationInSeconds;
 
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
     @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id")
     private RecipeEntity recipe;
 
     public InstructionEntity () {}
