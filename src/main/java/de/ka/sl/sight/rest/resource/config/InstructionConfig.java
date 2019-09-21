2ae5b692eb46896a1bbb67683fc8b405bf0dbad1
luthesebas <luthesebas@gmail.com>
2019-09-21 17:37:45 +0200

    Refactor resource patterns

--- src/main/java/de/ka/sl/sight/rest/resource/config/InstructionConfig.java
+++ src/main/java/de/ka/sl/sight/rest/resource/config/InstructionConfig.java
@@ -3,8 +3,11 @@ package de.ka.sl.sight.rest.resource.config;
 /** @author Sebastian Luther (@url(https://github.com/luthesebas)) */
 public class InstructionConfig extends ResourceConfig {
 
-    public static final String NAME = "instructions";
-    public static final String ROOT = "/" + NAME;
+    public static final String NAME = "instruction";
+    public static final String NAME_PLURAL = "instructions";
+
+    public static final String RESOURCE_NAME = NAME_PLURAL;
+    public static final String ROOT = "/" + RESOURCE_NAME;
 
     public static final String ID_NAME = "instructionId";
     public static final String ID_PATH = "/{" + ID_NAME + ":" + ID_PATTERN + "}";
--- src/main/java/de/ka/sl/sight/rest/resource/config/RecipeConfig.java
+++ src/main/java/de/ka/sl/sight/rest/resource/config/RecipeConfig.java
@@ -3,8 +3,11 @@ package de.ka.sl.sight.rest.resource.config;
 /** @author Sebastian Luther (@url(https://github.com/luthesebas)) */
 public class RecipeConfig extends ResourceConfig {
 
-    public static final String NAME = "recipes";
-    public static final String ROOT = "/" + NAME;
+    public static final String NAME = "recipe";
+    public static final String NAME_PLURAL = "recipes";
+
+    public static final String RESOURCE_NAME = NAME_PLURAL;
+    public static final String ROOT = "/" + RESOURCE_NAME;
 
     public static final String ID_NAME = "recipeId";
     public static final String ID_PATH = "/{" + ID_NAME + ":" + ID_PATTERN + "}";
