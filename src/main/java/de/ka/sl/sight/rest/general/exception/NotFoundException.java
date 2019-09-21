feda7b55c1eb9fe88d23e09fd972ea744f03ad66
luthesebas <luthesebas@gmail.com>
2019-09-21 17:34:45 +0200

    Update NotFoundException.java

--- src/main/java/de/ka/sl/sight/rest/general/exception/NotFoundException.java
+++ src/main/java/de/ka/sl/sight/rest/general/exception/NotFoundException.java
@@ -3,12 +3,12 @@ package de.ka.sl.sight.rest.general.exception;
 /** @author Sebastian Luther (@url(https://github.com/luthesebas)) */
 public class NotFoundException extends AppException {
 
-    public NotFoundException (Class clazz, long id) {
-        super("Could not find a " + clazz.getSimpleName().toLowerCase() + " with id " + id);
+    public NotFoundException (String resourceName, long id) {
+        super("Could not find a " + resourceName + " with id " + id);
     }
 
-    public NotFoundException (Class clazz) {
-        super("Could not find any " + clazz.getSimpleName().toLowerCase());
+    public NotFoundException (String resourceName) {
+        super("Could not find any " + resourceName);
     }
 
 }
